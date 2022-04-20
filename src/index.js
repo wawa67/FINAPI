8676d9c2bdbff3d417fb21f69cf0257d441b61dc
Wallace de Souza <wallace.silvadesouza67@gmail.com>
2022-04-20 07:57:17 -0300

    changes made to meet first requirement

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const app = express()
+
+app.use(express.json())
+
+const customers = []
+
+app.post("/account", (request, response)=>{
+    const {cpf, name} = request.body;
+    const id = uuidv4();
+
+    customers.push({
+        id,
+        name,
+        cpf,
+        statement: []
+    })
+
+    return response.status(201).json(customers);
+})
+
+app.listen(3333);
